7c83029986d79f694befbe0ba78d21fd29b679ea
DLS Student Temp <dlsstudenttemp@FED.CCLRC.AC.UK>
2017-07-04 16:00:21 +0100

    Removed comment and made to conform with PEP8

--- Prime number test (functional).py
+++ Prime number test (functional).py
@@ -1,10 +1,9 @@
 def testforprime(number):
-  for divisor in range(2,number):
-    #print number, divisor, number/divisor, 1.0*number/divisor, number/divisor == 1.0*number/divisor
-    if number/divisor == 1.0*number/divisor:
-      return False
-  return True
-for number in range(2,1001):
-  if testforprime(number) == True:
-    print number
-    print
+    for divisor in range(2, number):
+        if number/divisor == 1.0*number/divisor:
+            return False
+    return True
+for number in range(2, 101):
+    if testforprime(number) == True:
+        print number
+        print
